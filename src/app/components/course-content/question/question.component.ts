e91eedcd3795674e2843e12aef9d5d8f874d5f8e
DuvEva <evariste.dusingizimana@amalitechtraining.org>
2024-07-07 09:42:04 +0200

    display questions dynamically

--- src/app/components/course-content/question/question.component.ts
+++ src/app/components/course-content/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Question } from '@/types.common';
 
 @Component({
@@ -9,6 +9,7 @@ import { Question } from '@/types.common';
 export class QuestionComponent implements OnChanges {
   @Input() question: Question | undefined;
   @Input() index: number | undefined = 0;
+  @Output() currentQuestionChange = new EventEmitter<number>();
 
   ngOnChanges(changes: SimpleChanges) {
     if (
@@ -18,6 +19,7 @@ export class QuestionComponent implements OnChanges {
     ) {
       this.question = changes['question'].currentValue;
       this.index = changes['index'].currentValue + 1;
+      this.currentQuestionChange.emit(this.index);
       console.log('Current Question:', this.question, 'Index:', this.index);
     }
   }
--- src/app/components/course-content/qustion-tracker/qustion-tracker.component.ts
+++ src/app/components/course-content/qustion-tracker/qustion-tracker.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-qustion-tracker',
   templateUrl: './qustion-tracker.component.html',
-  styleUrl: './qustion-tracker.component.css'
+  styleUrls: ['./qustion-tracker.component.css'],
 })
-export class QustionTrackerComponent {
+export class QustionTrackerComponent implements OnChanges {
+  @Input() index: number | undefined = 0;
+  width: string = '0%';
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['index'] && this.index !== undefined) {
+      this.width = `${(this.index / 10) * 100}%`;
+      console.log('Tracker Width:', this.width);
+    }
+  }
 }
